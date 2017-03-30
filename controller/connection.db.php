ac7d88f47006dff898daf8f8550ad80d4ce54456
JKJUN <631642753@qq.com>
2017-03-30 20:27:55 +0800

    需求发布修改

--- controller/connection.db.php
+++ controller/connection.db.php
@@ -3,7 +3,7 @@ if (PHP_VERSION < '5.3.29') {
     exit('当前PHP版本低于5.3.29，请升级版本！');
 }
 
-define('DB_HOST', 'localhost');
+define('DB_HOST', '119.29.115.227');
 define('DB_USER', 'root');
-define('DB_PWD', '');
+define('DB_PWD', 'jkjun@12345');
 define('DB_NAME', 'db_four2nine');
